7284488a59dbad55a8f54ee64f6ce11b8c8ab52d
michaellei-e0ca <michaelleie0ca@gmail.com>
2023-10-29 12:47:31 -0700

    Finish submit button

---
+++ app/components/SubmitButton.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { experimental_useFormStatus as useFormStatus } from "react-dom";
+export default function SubmitButton() {
+  const { pending } = useFormStatus();
+  return (
+    <button disabled={pending} className="btn-primary">
+      {pending ? <span>Submitting...</span> : <span>Submit</span>}
+    </button>
+  );
+}
